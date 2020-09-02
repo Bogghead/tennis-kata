94c73cede5c799589a54b3c823536f315da87fdf
Bob Allen <bogghead@gmail.com>
2020-09-02 15:25:09 -0400

    Add Player Class - not used

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,9 +1,57 @@
 public class Player {
-     public String name;
-     public Integer points;
+    public static final int WINPOSSIBLETHRESHOLD = 4;
 
-     public Player(String name) {
-         this.name = name;
-     }
+    public final String name;
+    public Integer points = 0;
 
+    public Player(String playerName) {
+        name = playerName;
+    }
+
+    public void bumpScore() {
+        points += 1;
+    }
+
+    boolean isTied(Player opponent) {
+        return points.equals(opponent.points);
+    }
+
+    static String[] scoreNames = {"Love", "Fifteen", "Thirty", "Forty"};
+    String buildNonWinScore(Player opponent) {
+        return (isTied(opponent)) ? buildTiedScore() :  buildNonTiedScore(opponent);
+    }
+
+    private String buildNonTiedScore(Player opponent) {
+        return scoreNames[points] + "-" + scoreNames[opponent.points];
+    }
+
+    private String buildTiedScore() { 
+        return (cannotWin()) ? scoreNames[points] + "-All" : "Deuce";
+    }
+
+    String buildWinnableScore(Player opponent) {
+        if (isTied(opponent)) return "Deuce";
+        return isAdvantage(opponent)
+                ? "Advantage " + winningPlayName(opponent)
+                : "Win for " + winningPlayName(opponent);
+    }
+
+    private boolean isAdvantage(Player opponent) {
+        return Math.abs(points - opponent.points) == 1;
+    }
+
+    String winningPlayName(Player opponent) {
+        return (points > opponent.points) ? name : opponent.name;
+    }
+
+    boolean named(String playerName) {
+        return playerName.equals(name);
+    }
+    
+    boolean cannotWin() {
+        return ! canWin(); }
+
+    boolean canWin() {
+        return points >= WINPOSSIBLETHRESHOLD;
+    }
 }
